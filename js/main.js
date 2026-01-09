512a8eb1e628268847741016b8fc7fa2abf3f0b7
pareshkavale <68412586+pareshkavale@users.noreply.github.com>
2026-01-09 20:15:55 +0530

    added js files

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-if ($('.banner-img-slider').length) {
-    var swiper = new Swiper('.banner-img-slider', {
-        effect: 'fade',
-        loop: true,
-        slidesPerView: 1,
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
-    });
+$(function () {
+  $(".humberger-menu").click(function () {
+    $("body").addClass("menu-visible");
+  });
+  $(".side-menu .close-btn, .overlay").click(function () {
+    $("body").removeClass("menu-visible");
+  });
+});
 
-}
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() >= 150) {
+      $(".back-to-top").fadeIn();
+      $(".top-navigation").addClass("fixed");
+    } else {
+      $(".back-to-top").fadeOut();
+      $(".top-navigation").removeClass("fixed");
+    }
+  });
+});
 
+$(".scrollUp").on('click', function () {
+    $("html, body").animate({ scrollTop: 0 }, 0);
+    return false;
+});
 
 if ($('.testimonial-slider').length) {
     var swiper = new Swiper('.testimonial-slider', {
@@ -31,33 +43,3 @@ if ($('.testimonial-slider').length) {
     });
 
 }
-
-
-$(function () {
-    $(".sidebar-active").click(function () {
-        $("body").addClass("mobile-menu-visible");
-    });
-    $(".nav-menu-wrap .menu-backdrop, .nav-menu-wrap .close-btn, .overlay").click(function () {
-        $("body").removeClass("mobile-menu-visible");
-    });
-
-})
-
-
-$(function () {
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop() >= 150) {
-            $(".back-to-top").fadeIn();
-            $(".header-navigation").addClass("fixed");
-        } else {
-            $(".back-to-top").fadeOut();
-            $(".header-navigation").removeClass("fixed");
-        }
-    });
-
-})
-
-$(".scrollUp").on('click', function () {
-    $("html, body").animate({ scrollTop: 0 }, 0);
-    return false;
-});
